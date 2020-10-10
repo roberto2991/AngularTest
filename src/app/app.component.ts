a0cbab255ff6d77eb459eec3425e675942eecfb7
roberto2991 <robertomasellis@gmail.com>
2020-10-10 10:45:59 +0200

    prima commit di prova angular learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyApp';
+  url: string;
+  visible = false;
+  users=['mario','valeria','roberto'];
+
+  ClickString(event: MouseEvent){
+    console.log('hai cliccato',event)
+  }
+
+  pressHandler(event: KeyboardEvent){
+    const target = event.target as HTMLInputElement
+    console.log('hai digitato su tastiera', target.value)
+  }
+
+  DoVisible(){
+    if (this.visible){
+      this.visible = false;
+    }else{
+      this.visible = true;
+    }
+    
+  }
+
+  Loadimg(){
+    this.url = 'https://www.udemy.com/staticx/udemy/images/v6/logo-coral-light.svg';
+  }
+  Unloadimg(){
+    this.url =  null;
+  }
 }
