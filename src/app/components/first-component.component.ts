1ee6958ba0eb1b906fa3962e969721ae75651130
roberto2991 <robertomasellis@gmail.com>
2020-10-10 20:35:14 +0200

    Using of interface

--- src/app/components/first-component.component.ts
+++ src/app/components/first-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from './../model/users';
 
 @Component({
   selector: 'app-first-component',
@@ -10,11 +11,12 @@ export class FirstComponentComponent implements OnInit {
   date = Date.now();
   decimal = 10.23230432;
 
-  users=[{id:'1',
+  users: Users[] = [
+        {id: 1,
           name:'mario',
           testo:'mario ie nu bell waglion e fasc tand cos bell'
         },
-        {id:'2',
+        {id: 2,
           name:'valeria',
           testo:'valeria ie na bell wagned e fasc tand '
         }
